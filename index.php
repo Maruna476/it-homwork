eb84b81a44af4f50cdf249a71eb75c691b0227c5
Vovk Maruna <m93407263@gmail.com>
2026-05-20 17:31:32 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,60 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['name']) || !isset($_SESSION['email'])) {
-    header("Location: register.php");
-    exit();
-} else {
-    header("Location: profile.php");
-    exit();
+if (isset($_GET['logout'])) {
+    session_destroy();
+    header("Location: index.php");
 }
+
+if (isset($_POST['login']) && isset($_POST['password'])) {
+
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    if ($login == "admin" && $password == "admin") {
+        $_SESSION['user'] = $login;
+    } else {
+        $error = "Неправильний логін або пароль";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<?php if (isset($_SESSION['user'])) { ?>
+
+    <h2>Привіт, <?php echo $_SESSION['user']; ?></h2>
+
+    <p>Ваш IP: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+
+    <a href="?logout=1">Вийти</a>
+
+<?php } else { ?>
+
+    <form method="post">
+
+        <input type="text" name="login" placeholder="Логін">
+        <br><br>
+
+        <input type="password" name="password" placeholder="Пароль">
+        <br><br>
+
+        <button type="submit">Увійти</button>
+
+    </form>
+
+    <?php
+    if (isset($error)) {
+        echo "<p>$error</p>";
+    }
+    ?>
+
+<?php } ?>
+
+</body>
+</html>
